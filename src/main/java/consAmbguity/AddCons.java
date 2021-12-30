e0010ffd03f9f8d465aa4578402969c94aae3a8d
kedar.sedai <kedar.sedai05@gmail.com>
2021-12-30 12:44:39 +0545

    Added Constructor Ambguity

--- src/main/java/consAmbguity/AddCons.java
+++ src/main/java/consAmbguity/AddCons.java
@@ -1,2 +1,32 @@
-package consAmbguity;public class AddCons {
+package consAmbguity;
+
+public class AddCons {
+    private int a;
+    private int b;
+
+    public AddCons(double a, double b) {
+        System.out.println("FROM DOUBLE");
+        this.a = (int)a;
+        this.b = (int)b;
+    }
+
+    //CONS OVERLOADING
+    public AddCons(int a, int b) {
+        System.out.println("FRoM INTEGER");
+        this.a = a;
+        this.b = b;
+    }
+
+    public AddCons(String a, String b) {
+        System.out.println("FRoM STRING");
+        this.a = Integer.parseInt(a);
+        this.b = Integer.parseInt(b);
+    }
+
+
+    public void sum(){
+        int c = this.a + this.b;
+        System.out.println("Calling SUM METHOD");
+        System.out.println("Sum is : " + c);
+    }
 }
--- src/main/java/consAmbguity/OutputCons.java
+++ src/main/java/consAmbguity/OutputCons.java
@@ -1,2 +1,12 @@
-package consAmbguity;public class OutputCons {
+package consAmbguity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OutputCons {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("consAmgConfig.xml");
+        AddCons addCons = (AddCons) context.getBean("consAmg");
+        addCons.sum();
+    }
 }
